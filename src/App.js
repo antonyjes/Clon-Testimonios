e8dec3c36afb73ce6d310e1e6d29230d5f20baf3
antonyjes <72809149@continental.edu.pe>
2022-11-02 18:51:22 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Testimonio from "./componentes/Testimonio";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimonio
+          nombre="Aegon"
+          country="Suecia"
+          imagen="1"
+          cargo="Ingeniero de Software"
+          empresa="Spotify"
+          testimonio="It's scary to change careers. I only gained confidence that I could code by working through the hundreds of hours of free lessons on freeCodeCamp. Within a year I had a six-figure job as a Software Engineer. freeCodeCamp changed my life."
+        />
+        <Testimonio
+          nombre="Rhaenyra"
+          country="USA"
+          imagen="2"
+          cargo="Ingeniera de Software"
+          empresa="Amazon"
+          testimonio="freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company."
+        />
+        <Testimonio
+          nombre="Eliana"
+          country="Rusia"
+          imagen="3"
+          cargo="Ingeniera de Software"
+          empresa="Ebay"
+          testimonio="I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify."
+        />
+      </div>
     </div>
   );
 }
